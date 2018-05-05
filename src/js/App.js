94e45790fb9abddf4d3fe792a0b8049b635a8233
Mizuno Sugu <mimizunosuguru@gmail.com>
2018-05-05 11:51:59 +0900

    Todoアプリ雛形完成

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React, { Component } from 'react';
+import TodoList from './TodoList';
+import '../stylesheets/app.css'
 
-const render = () => {
-  ReactDom.render(<div>いえーーい</div>,
-    document.querySelector('.app'),
-  );
-};
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: "Hello, React!",
+          desc: "Reactはじめました",
+          done: false
+        },
+        {
+          id: 2,
+          title: "Hello, React!",
+          desc: "Reduxtはじめました",
+          done: false
+        },
+      ]
+    }
+  }
 
-render();
+  render() {
+    return (
+      <div className="app">
+        <h1>todoアプリ作って見た</h1>
+        <TodoList
+          todos={this.state.todos}
+        />
+      </div>
+    );
+  }
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var path = require('path');
 module.exports = {
 	context: path.join(__dirname, "src"),
 	devtool: debug ? "inline-sourcemap" : false,
-	entry: "./js/App.js",
+	entry: "./js/index.js",
 	resolve: {
 		extensions: ['', '.js', '.jsx']
 	},
