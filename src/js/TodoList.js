2733f63f7efc11a1b2f9460f6aad85ebc691ee50
Mizuno Sugu <mimizunosuguru@gmail.com>
2018-05-05 13:37:42 +0900

    カウンター&インプット追加

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -2,6 +2,22 @@ import React, {Component } from 'react';
 import Todo from './Todo';
 
 class TodoList extends Component {
+  constructor() {
+    super()
+    this.state={
+      count: 0,
+      value: "",
+    }
+  }
+
+  plus() {
+    this.setState({count: this.state.count + 1})
+  }
+
+  onChange(e) {
+    this.setState({value: e.target.text})
+  }
+
   render() {
     const todos = this.props.todos.map( todo =>
       <Todo
@@ -11,9 +27,23 @@ class TodoList extends Component {
     )
 
     return(
-      <ul>
-        {todos}
-      </ul>
+      <div>
+        <div>
+          <h2>カウンター</h2>
+          <p>{this.state.count}</p>
+          <button onClick={this.plus.bind(this)}>+</button>
+          <h2>インプット</h2>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.onChange.bind(this)}
+          />
+          <p>{this.state.text}</p>
+        </div>
+        <ul>
+          {todos}
+        </ul>
+      </div>
     )
   }
 }
